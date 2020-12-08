835bc5ed11e926cb4b0b875ddddfd0eb2097c18e
Pablo Ferreyra <pabloeferreyra@gmail.com>
2020-12-07 21:10:16 -0300

    database created

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -3,6 +3,7 @@ using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using Traders.Models;
 
 namespace Traders.Data
 {
@@ -11,6 +12,13 @@ namespace Traders.Data
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
+        }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            builder.Entity<MovementsViewModel>()
+            .Property(b => b.DateMov)
+            .HasDefaultValueSql("getdate()");
         }
     }
 }
