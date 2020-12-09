62301a3275a6de25d734fa46900e0245320496e0
Pablo Ferreyra <pabloeferreyra@gmail.com>
2020-12-08 21:15:19 -0300

    Movements (initial) and badges

---
+++ wwwroot/js/badges.js
@@ -0,0 +1,45 @@
+﻿function Create(urlAction) {
+    var form = $('#__AjaxAntiForgeryForm');
+    var token = $('input[name="__RequestVerificationToken"]', form).val();
+    $.ajax({
+        type: "POST",
+        url: urlAction,
+        data: {
+            __RequestVerificationToken: token,
+            Name: $("#badgeName").val()
+        },
+        dataType: "json",
+        contentType: "application/json"
+    });
+}
+
+function Delete(urlAction, id) {
+    var form = $('#__AjaxAntiForgeryForm');
+    var token = $('input[name="__RequestVerificationToken"]', form).val();
+    $.ajax({
+        type: "POST",
+        url: urlAction,
+        data: {
+            __RequestVerificationToken: token,
+            id: id
+        },
+        success: function (result) {
+            $("#TurnsPartial").html(result);
+        },
+        error: function (req, status, error) {
+        }
+    });
+}
+
+function ConfirmDelete(uniqueId, isAccessedClicked) {
+    var accessedSpan = 'accessedSpan_' + uniqueId;
+    var confirmaccessedSpan = 'confirmaccessedSpan_' + uniqueId;
+
+    if (isAccessedClicked) {
+        $('#' + accessedSpan).hide();
+        $('#' + confirmaccessedSpan).show();
+    } else {
+        $('#' + accessedSpan).show();
+        $('#' + confirmaccessedSpan).hide();
+    }
+}
