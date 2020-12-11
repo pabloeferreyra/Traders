28264f06ba6e4d6b445c9c19d81f42676c3924c7
Pablo Ferreyra <pabloeferreyra@gmail.com>
2020-12-10 22:41:44 -0300

    fix balances

--- Models/BankAccountsViewModel.cs
+++ Models/BankAccountsViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,6 +9,7 @@ namespace Traders.Models
     public class BankAccountsViewModel
     {
         public Guid Id { get; set; }
+        [Display(Name = "Nombre"), Required]
         public String Name { get; set; }
         public Decimal Amount { get; set; }
         public ICollection<MovementsViewModel> Movements { get; set; }
